5dc54ef8d1387f5e21e5e572e556f9dd8d4118ff
Nargiz <ngzkanny17@gmail.com>
2020-04-11 15:38:10 +0600

    fragments and viewpagers edit

---
+++ app/src/main/java/com/example/ourmovie/viewpagers/CustomViewPager.kt
@@ -0,0 +1,34 @@
+package com.example.movieapp.ViewPager
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+class CustomViewPager: ViewPager {
+    private var swipable = false
+
+    constructor(context:Context?):super(context!!){}
+    constructor(context:Context?, attrs: AttributeSet?):super(
+        context!!,
+        attrs
+    ){
+        swipable = true
+    }
+
+    override fun onTouchEvent(event: MotionEvent):Boolean{
+        return if(swipable){
+            super.onTouchEvent(event)
+        }else{
+            false
+        }
+    }
+
+    override fun onInterceptTouchEvent(ev:MotionEvent):Boolean{
+        return if(swipable) super.onInterceptTouchEvent(ev) else false
+    }
+
+    fun setSwipable(swipe:Boolean){
+        swipable = swipe
+    }
+}
