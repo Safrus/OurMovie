b276cf2a660b222873e289f43ebd48a396d289a5
Safrus <rus.safarov222@gmail.com>
2020-04-11 15:16:56 +0600

    Revert "first commit in develop branch"

---
+++ app/src/main/java/com/example/ourmovie/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ourmovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//develop
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
